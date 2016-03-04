c82329039e38f653497321745f6385b0d04e47f6
root <root@beaglebone.localdomain>
2016-03-04 08:26:18 +0000

    fixed bug in updateIP that was adding whitespace, blowing up badass.js, reverted badass.js

--- updateIP.py
+++ updateIP.py
@@ -1,31 +1,24 @@
 import os
-import fileinput
-
-os.system("ifconfig > ip.txt")
-
-ip_file = open("ip.txt", "r")
-
-ip_lines = ip_file.readlines()
-
-ip_address = ""
-
-wlan_found = False
-
-for line in ip_lines:
-
+import fileinput 
+os.system("ifconfig > ip.txt") 
+ip_file = open("ip.txt", "r") 
+ip_lines = ip_file.readlines() 
+ip_address = "" 
+wlan_found = False 
+for line in ip_lines: 	
 	if wlan_found == True:
-		ip_address = line.split(":")[1].split(" ")[0]
-		
-		break
-	if "wlan0" in line:
-		wlan_found = True
-
-if ip_address != "":
-
-	for line in fileinput.input("badass.js", inplace=True):
-	    if "var socketaddyLocal" in line:
-	    	print "var socketaddyLocal = \"ws://{0}:9001\"".format(ip_address)
-	    else:
-	    	print line
+		ip_address = line.split(":")[1].split(" ")[0]		
+		break 
+if "wlan0" in line:
+	wlan_found = True 
+	if ip_address != "": 	
+		for line in fileinput.input("badass.js", inplace=True):
+			if "var socketaddyLocal" in line:
+				print "var socketaddyLocal = \"ws://{0}:9001\"".format(ip_address)
+			else:
+				line = line.replace("\n", "")
+				if len(line) > 1:
+					print line
+				
 ip_file.close()
 os.system("rm ip.txt")
